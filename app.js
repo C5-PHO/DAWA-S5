47e2089cd89681fea014eb7a96e5ea5e436645bf
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-09-19 08:55:46 -0500

    feat: importacion de rutas

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(express.json()); // Para leer JSON en las solicitudes
 app.use(cors()); // Permitir solicitudes de otros dominios
 app.use(morgan("dev")); // detalles de cada petición
 
+//importamos los módulos de rutas
+const ticketRoutes = require("./routes/ticket.routes");
+const notificationRoutes = require("./routes/notification.routes");
+
+//rutas bases
+app.use("/tickets", ticketRoutes);
+app.use("/notifications", notificationRoutes);
+
 // Mensaje de prueba en la raíz
 app.get("/", (req, res) => {
   res.send("¡Bienvenido a la API RESTful!");
